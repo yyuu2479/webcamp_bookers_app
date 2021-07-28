4000e9e7995be2a3c3b59df4e96202da07270a46
yyuu2479 <yyuu2479@gmail.com>
2021-07-28 05:29:18 +0000

    BOOKERS

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,13 +7,27 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   # プライベート
