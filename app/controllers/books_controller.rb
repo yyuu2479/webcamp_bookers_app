55312fd169f3985e659c10025efc56e2c1e2f90f
yyuu2479 <yyuu2479@gmail.com>
2021-07-30 09:45:19 +0000

    BOOKERS

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,20 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all.order(id: :desc)
+    @books = Book.all
     @book = Book.new
   end
 
   def create
-    @books = Book.all.order(id: :desc)
+    @books = Book.all
     @book = Book.new(book_params)
-    
+
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :index
     end
-      
+
   end
 
   def show
@@ -26,9 +26,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
